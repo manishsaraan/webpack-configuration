4f73cdca0caa51221db49b2832658e6dc71678ae
manishsaraan <manish021js@gmail.com>
2018-02-08 10:36:25 +0530

    setting up dev configurations in webpack

--- exercise2/webpack.config.js
+++ exercise2/webpack.config.js
@@ -9,7 +9,12 @@ module.exports = {
 	},
 	output : {
 		filename : '[name].bundle.js',
-		path: path.resolve(__dirname , 'dist')
+		path: path.resolve(__dirname , 'dist'),
+		publicPath : '/'
+	},
+	devtool : 'inline-source-map',
+	devServer : {
+		contentBase : './dist/'
 	},
 	module : {
 		rules : [
